2caf94fe354189d3c2f03fa8164badc97fa13f4b
Arman3747 <dailyarman7@gmail.com>
2025-07-13 21:28:07 +1000

    First Commit !

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-7xl'>Blood Connect HHH </h1>
+      <p className='text-3xl text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusantium aliquid soluta, ab facilis nesciunt quae quam? Eaque aliquid sunt at, ipsa similique qui itaque! Corporis itaque libero repellat cupiditate.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, repellendus saepe? Eos molestias voluptas perferendis vero dolores, accusantium laudantium itaque inventore dolor iure quis nobis ea corporis distinctio dignissimos quibusdam exercitationem vel, aut aspernatur sed ex ducimus! Recusandae iste rerum veniam architecto voluptatem consequuntur molestiae quam sint explicabo fugit dolore facere ex doloribus, accusantium eos porro totam error. Non ad eum sequi, explicabo ipsa reprehenderit earum, omnis blanditiis sapiente iusto id officia incidunt vero nihil quae iste. Nobis distinctio impedit iure totam, explicabo dicta quis pariatur amet voluptas iusto autem magni dolore neque quia, non corrupti tenetur praesentium fugiat rerum.
+      </p>
+      <img src="https://i.ibb.co/gLZK6Snn/blood-connect-white-logo.png" alt="" />
+      <img src="https://i.ibb.co/gB5d8mS/blood-connect-black-logo.png" alt="" />
+
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
